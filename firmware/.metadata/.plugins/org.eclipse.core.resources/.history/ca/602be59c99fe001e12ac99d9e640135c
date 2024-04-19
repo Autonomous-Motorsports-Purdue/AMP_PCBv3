7fbe433e1c9d09206bf8afc29aedaf87f9e9d01c
Ben Owen <ben@bensuniverse.com>
2024-04-19 18:45:27 -0400

    Change existing driver format and setup state machine skeleton

---
+++ firmware/.metadata/.plugins/org.eclipse.core.resources/.history/ca/602be59c99fe001e12ac99d9e640135c
@@ -0,0 +1,9 @@
+#ifndef __EBRAKE_H
+#define __EBRAKE_H
+
+void Driver_EBrake_Init();
+void Driver_EBrake_Extend();
+void Driver_EBrake_Retract();
+void Driver_EBrake_Off();
+
+#endif
--- firmware/AMP_PCBv3/Debug/Core/Src/main.d
+++ firmware/AMP_PCBv3/Debug/Core/Src/main.d
@@ -1,4 +1,5 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/main.o: ../Core/Src/main.c ../Core/App/Inc/driver_ebrake.h \
+ ../Core/App/Inc/driver_fan.h ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Core/Inc/stm32f4xx_hal_conf.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
@@ -31,7 +32,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- ../Core/App/Inc/LoRa.h ../Core/App/Inc/fan.h ../Core/App/Inc/ebrake.h
+ ../Core/App/Inc/LoRa.h
+../Core/App/Inc/driver_ebrake.h:
+../Core/App/Inc/driver_fan.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -66,5 +69,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
 ../Core/App/Inc/LoRa.h:
-../Core/App/Inc/fan.h:
-../Core/App/Inc/ebrake.h:
